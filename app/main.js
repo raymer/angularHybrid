d9582b5bf24914b4f81546ca0c976060473581f6
Jon Raymer <jon.raymer@lssinc.com>
2017-05-14 14:40:59 -0500

    delete webpack generated js files

--- app/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var static_1 = require('@angular/upgrade/static');
-var app_module_1 = require('./app.module');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule).then(function (platformRef) {
-    var upgrade = platformRef.injector.get(static_1.UpgradeModule);
-    upgrade.bootstrap(document.documentElement, ['myApp']);
-});
-//# sourceMappingURL=main.js.map
